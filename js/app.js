7c4610acc3081d6763cc41a12ed67ba26b2ddca3
Ron Shapiro <shapiro.rd@gmail.com>
2012-12-10 01:20:04 -0500

    Resolved conflict

--- js/app.js
+++ js/app.js
@@ -1,18 +1,24 @@
-angular.module('ProjectSALL', ['SallServices']).
+angular.module('ProjectSALL', ['SallServices', 'ngSanitize']).
 	config(function($routeProvider) {
 		$routeProvider.
 			when('/', { controller: 'ListsController', templateUrl: 'views/lists.html' }).
 			when('/searchresults', { controller: 'SearchResultsController', templateUrl: 'views/searchresults.html' }).
 			when('/help', { controller: 'HelpController', templateUrl: 'views/help.html' }).
 			when('/details', { controller: 'DetailsController', templateUrl: 'views/details.html' }).
+      when('/reset', { controller: 'ResetController', templateUrl: 'views/lists.html'}).
 			otherwise({redirectTo:'/'});
 	});
 
 var SavedLists;
 
 window.onload = function(){
-  if (localStorage.lists !== 'undefined')
+  if (localStorage.lists === '"undefined"' ||
+      localStorage.lists === 'undefined' ||
+      _.isEmpty(JSON.parse(localStorage.lists)))
+    SavedLists = [{name:'Initial List', venues: []}];    
+  else
     SavedLists = JSON.parse(localStorage.lists);
+
 };
 window.onbeforeunload = function(){
   localStorage.lists = JSON.stringify(SavedLists);
