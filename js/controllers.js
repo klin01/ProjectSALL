2babde8d2ffb6f0f0593aa3047f91346d19c2c67
Kevin Lin <lin.kevin.01@gmail.com>
2012-12-09 21:58:30 -0500

    Added and tested building business query to API

--- js/controllers.js
+++ js/controllers.js
@@ -1,9 +1,16 @@
-function ListsController ($scope, YelpAPI, OAuthRequest) {
+function ListsController ($scope, YelpAPI, OAuthRequest, SearchParse, BusinessParse) {
 	var test = OAuthRequest.buildSearchUrl('chinese');
-	var test2;
+	var test2 = OAuthRequest.buildBusinessUrl('new-kam-hing-coffee-shop-new-york');
+
+	var samplesearchresults;
 	YelpAPI.query({ url: encodeURIComponent(test) }, function(rsp) {
-		test2 = rsp;
-    console.log(rsp);
+    	samplesearchresults = SearchParse.scrub(rsp);
+    	console.log(samplesearchresults);
+	});
+	var samplebusinessresult;
+	YelpAPI.query({ url: encodeURIComponent(test2) }, function(rsp) {
+    	samplebusinessresult = BusinessParse.scrub(rsp);
+    	console.log(samplebusinessresult);
 	});
 }
 
